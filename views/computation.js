e5ea59c380fac1abca5f32a1cc4d2103835ec544
Adithyakrishna9 <98135664+Adithyakrishna9@users.noreply.github.com>
2022-03-30 10:32:31 -0500

    added the computation.js

---
+++ views/computation.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  const x= req.query.x || getRandomArbitrary(0, 60);
+  var y1 = Math.atan2(x);
+  var y2 = Math.atanh(x);
+  var y3 = Math.cbrt(x);
+  function getRandomArbitrary(min, max) {
+    return Math.floor(Math.random() * (max - min) + min);
+  }
+  res.send(`Math.atan2(${x}) is ${y1} ; Math.atanh(${x}) is ${y2} ; Math.cbrt(${x}) is ${y3}\n`);
+});
+
+module.exports = router;
